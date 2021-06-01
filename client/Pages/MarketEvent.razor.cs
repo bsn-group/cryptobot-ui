5e3752f2e3c09fce2e087c04f663740dd2a9a1ca
lambda125 <927405+lambda125@users.noreply.github.com>
2021-06-01 13:10:29 +1000

    Fixing date timezones (#2)

--- client/Pages/MarketEvent.razor.cs
+++ client/Pages/MarketEvent.razor.cs
@@ -18,5 +18,66 @@ namespace CryptobotUi.Pages
         {
             
         }
+
+        IEnumerable<CryptobotUi.Models.Cryptodb.MarketEvent> _getMarketEventsResult;
+        protected IEnumerable<CryptobotUi.Models.Cryptodb.MarketEvent> getMarketEventsResult
+        {
+            get
+            {
+                return _getMarketEventsResult;
+            }
+            set
+            {
+                if (!object.Equals(_getMarketEventsResult, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "getMarketEventsResult", NewValue = value, OldValue = _getMarketEventsResult };
+                    _getMarketEventsResult = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        int _getMarketEventsCount;
+        protected int getMarketEventsCount
+        {
+            get
+            {
+                return _getMarketEventsCount;
+            }
+            set
+            {
+                if (!object.Equals(_getMarketEventsCount, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "getMarketEventsCount", NewValue = value, OldValue = _getMarketEventsCount };
+                    _getMarketEventsCount = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        protected async System.Threading.Tasks.Task OnGridLoadData(LoadDataArgs args)
+        {
+            args.OrderBy = string.IsNullOrWhiteSpace(args.OrderBy) ? "event_time desc" : args.OrderBy;
+
+            try
+            {
+                var cryptodbGetMarketEventsResult = await Cryptodb.GetMarketEvents(filter:$"{args.Filter}", orderby:$"{args.OrderBy}", top:args.Top, skip:args.Skip, count:args.Top != null && args.Skip != null);
+                getMarketEventsResult = 
+                    cryptodbGetMarketEventsResult.Value
+                    .Select(e => {
+                        e.event_time = e.event_time.ToLocalTime();
+                        return e;
+                    })
+                    .AsODataEnumerable();
+
+                getMarketEventsCount = cryptodbGetMarketEventsResult.Count;
+            }
+            catch (System.Exception cryptodbGetMarketEventsException)
+            {
+                NotificationService.Notify(new NotificationMessage(){ Severity = NotificationSeverity.Error,Summary = $"Error",Detail = $"Unable to load MarketEvents" });
+            }
+        }
     }
 }
--- client/Pages/MarketEvent.razor.designer.cs
+++ client/Pages/MarketEvent.razor.designer.cs
@@ -48,44 +48,6 @@ namespace CryptobotUi.Pages
         protected CryptodbService Cryptodb { get; set; }
         protected RadzenGrid<CryptobotUi.Models.Cryptodb.MarketEvent> grid0;
 
-        IEnumerable<CryptobotUi.Models.Cryptodb.MarketEvent> _getMarketEventsResult;
-        protected IEnumerable<CryptobotUi.Models.Cryptodb.MarketEvent> getMarketEventsResult
-        {
-            get
-            {
-                return _getMarketEventsResult;
-            }
-            set
-            {
-                if (!object.Equals(_getMarketEventsResult, value))
-                {
-                    var args = new PropertyChangedEventArgs(){ Name = "getMarketEventsResult", NewValue = value, OldValue = _getMarketEventsResult };
-                    _getMarketEventsResult = value;
-                    OnPropertyChanged(args);
-                    Reload();
-                }
-            }
-        }
-
-        int _getMarketEventsCount;
-        protected int getMarketEventsCount
-        {
-            get
-            {
-                return _getMarketEventsCount;
-            }
-            set
-            {
-                if (!object.Equals(_getMarketEventsCount, value))
-                {
-                    var args = new PropertyChangedEventArgs(){ Name = "getMarketEventsCount", NewValue = value, OldValue = _getMarketEventsCount };
-                    _getMarketEventsCount = value;
-                    OnPropertyChanged(args);
-                    Reload();
-                }
-            }
-        }
-
         protected override async System.Threading.Tasks.Task OnInitializedAsync()
         {
             await Load();
@@ -97,19 +59,7 @@ namespace CryptobotUi.Pages
 
         protected async System.Threading.Tasks.Task Grid0LoadData(LoadDataArgs args)
         {
-            args.OrderBy = string.IsNullOrWhiteSpace(args.OrderBy) ? "event_time desc" : args.OrderBy;
-
-            try
-            {
-                var cryptodbGetMarketEventsResult = await Cryptodb.GetMarketEvents(filter:$"{args.Filter}", orderby:$"{args.OrderBy}", top:args.Top, skip:args.Skip, count:args.Top != null && args.Skip != null);
-                getMarketEventsResult = cryptodbGetMarketEventsResult.Value.AsODataEnumerable();
-
-                getMarketEventsCount = cryptodbGetMarketEventsResult.Count;
-            }
-            catch (System.Exception cryptodbGetMarketEventsException)
-            {
-                NotificationService.Notify(new NotificationMessage(){ Severity = NotificationSeverity.Error,Summary = $"Error",Detail = $"Unable to load MarketEvents" });
-            }
+            await OnGridLoadData(args);
         }
 
         protected async System.Threading.Tasks.Task GridDeleteButtonClick(MouseEventArgs args, dynamic data)
